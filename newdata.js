b8f1bcf05b602cdf2ee8c6e06bd291f4dc1173b2
Shubhchaurasia <87860052+Shubhchaurasia@users.noreply.github.com>
2024-07-14 20:27:21 +0530

    Create newdata.js

---
+++ newdata.js
@@ -0,0 +1,18 @@
+let questions= [
+  {opt1:"Elephant",opt2: "Dog",opt3: "Whale",answer: "Elephant",image:"https://i.ibb.co/sKRJj26/Elephant.jpg"},
+  {opt1:"Cow",opt2:  "Goat",opt3: "Crocodile",answer: "Crocodile",image:"https://i.ibb.co/3cT7Bhy/croc.jpg"},
+  {opt1:"Lion", opt2: "Giraffe", opt3:"Cow",answer: "Giraffe",image:"https://i.ibb.co/tcrNncc/giraffe.jpg"},
+  {opt1:"Gorilla",opt2:  "Parrot",opt3: "Whale",answer: "Gorilla",image:"https://i.ibb.co/4mwkh8M/gorilla.jpg"},
+  {opt1:"Horse",opt2:  "Koala", opt3:"Polarbear",answer: "Koala",image:"https://i.ibb.co/F5HFPXC/koala.jpg"},
+  {opt1:"Duck", opt2: "Koala", opt3:"Tiger",answer: "Tiger",image:"https://i.ibb.co/0mm0j1W/tiger.jpg"},
+  {opt1:"Polarbear", opt2: "Whale",opt3:"Crocodile",answer: "Whale",image:"https://i.ibb.co/B4h6XxR/whale.jpg"},
+  {opt1:"Polarbear",opt2:  "Koala",opt3: "Dog",answer: "Polarbear",image:"https://i.ibb.co/bmynHNK/polarbear.jpg"},
+  {opt1:"Goat",opt2:  "Crocodile",opt3:"Cow",answer: "Cow",image:"https://i.ibb.co/ZX0Ttmr/cow.jpg"},
+  {opt1:"Rabbit", opt2: "Goat",opt3: "Cow",answer: "Goat",image:"https://i.ibb.co/2YMJkRN/goat.jpg"},
+  {opt1:"Lion",opt2:  "Dog",opt3: "Whale",answer: "Lion",image:"https://i.ibb.co/gWYnJJb/lion.jpg"},
+  {opt1:"Duck",opt2:  "Parrot",opt3:"Polarbear",answer: "Parrot",image:"https://i.ibb.co/DGxzYdp/parrot.jpg"},
+  {opt1:"Parrot", opt2: "Koala",opt3: "Rabbit",answer: "Rabbit",image:"https://i.ibb.co/WcRLc46/rabbit.jpg"},
+  {opt1:"Polarbear",opt2:  "Bear",opt3:"Parrot",answer: "Bear",image:"https://i.ibb.co/hHS8LJr/bear.jpg"},
+  {opt1:"Duck", opt2:"Koala",opt3: "Crocodile",answer:"Duck",image:"https://i.ibb.co/f80F5s3/duck.jpg"},
+]
+module.exports = {questions};
